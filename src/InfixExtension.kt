f6198c7861ba6a8600acbd2a7f7d6d8a57710e48
Vedansh Bhardwaj <vedansh.ved@anywhere.co>
2022-04-10 20:51:18 +0530

    InfixExtensionOperatorOverloading

---
+++ src/InfixExtension.kt
@@ -0,0 +1,34 @@
+fun main (){
+    var nb = Notebook("Classmate",  50, true)
+    println(nb)
+    println("________________________________________________________________")
+    nb.addPages(250)
+    println("After adding pages through extension function")
+    println(nb)
+    println("________________________________________________________________")
+
+    var nb1 = Notebook("Nova", 1000, true)
+    println("Combined Pages in Notebook 1 and 2 -:> ${nb addP nb1}")
+    println("________________________________________________________________")
+
+    var concatenatedCompanyName = nb + nb1
+    println("The combined company name of both notebooks is $concatenatedCompanyName")
+    println("________________________________________________________________")
+
+}
+
+//Extension Function.....
+fun Notebook.addPages(morePages : Int){
+    this.pages+=morePages
+}
+
+//Infix Function.......
+//      -> Must be either a member or an extension function
+infix fun Notebook.addP (n : Notebook) : Int{
+    return this.pages + n.pages
+}
+//Operator Overloading.......
+//      -> Must be either a member or an extension function
+operator fun Notebook.plus (n : Notebook) : String {
+    return this.company+n.company
+}
